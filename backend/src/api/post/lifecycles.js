9e91a58a190ebeb62067e114b8270c1ed3bd6c69
Daniel Abati <danielabatibabatunde1@gmail.com>
2024-04-20 13:05:43 +0100

    Building Base version for my Blog

---
+++ backend/src/api/post/lifecycles.js
@@ -0,0 +1,26 @@
+/**
+ * @description working with reading time of users
+ */
+
+const readingTime = require("reading-time");
+
+module.exports = {
+  async beforeCreate(event) {
+    console.log("########## BEFORE CREATE ##########");
+    if (event.params.data.content) {
+      event.params.data.readingTime =
+        // @ts-ignore
+        readingTime(event.params.data.content)?.text || null;
+    }
+  },
+
+  async beforeUpdate(event) {
+    // @ts-ignore
+    console.log("########## BEFORE UPDATE ##########");
+    if (event.params.data.content) {
+      event.params.data.readingTime =
+        // @ts-ignore
+        readingTime(event.params.data.content)?.text || null;
+    }
+  },
+};
